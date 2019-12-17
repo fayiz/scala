fe0d53092b2a5ea28948d7492e215ebf11f62e2b
fayiz <fayiz.usman@gmail.com>
2019-12-17 17:05:04 +0400

    Lists and Sets

---
+++ ListAndSet.scala
@@ -0,0 +1,28 @@
+package fayiz
+
+object ListAndSet{
+  def main(args: Array[String]){
+    println("Set Example");
+    var setv = Set(1, 2, 3, 4, 5)
+    setv = setv ++: Set(5, 6)
+    println("Set : " + setv)
+    println("Head : " + setv.head)
+    println("Tail : " + setv.tail)
+    println(setv.tail.head)
+    println(setv.tail.tail)
+    
+    println("\nList Example");
+    var listv = List(1, 2, 3, 4, 5)
+    println(listv)
+    listv = listv ++: List(5, 6, 7)
+    println(listv)
+    println("Second Element: " + listv(1))
+    println( "List Size: " + listv.size)
+    println("Head : " + listv.head)
+    println("Tail : " + listv.tail)
+    for(x <- listv){
+      println(x)
+    }
+    println("Distinct : " + listv.distinct)
+  }
+}
